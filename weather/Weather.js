284daeafae22b605aeff7e5c75edb15a26e2fac1
Fawaztarar <fawaztarar@gmail.com>
2024-01-19 21:04:41 +0000

    Your commit message here

--- weather/Weather.js
+++ weather/Weather.js
@@ -1,3 +1,4 @@
+//Weather.js
 // const WeatherUI = require('./WeatherUI');
 const WeatherClient = require('./WeatherClient');
 
@@ -16,41 +17,41 @@ class Weather {
   getWeatherData() {
     return this.weatherData
   }
+
+  async refreshWeatherData() {
+    try {
+      const weatherData = await this.fetchWeatherData(this.city);
+      this.weatherUI.displayWeather(weatherData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async compareWith(city) {
+    try {
+      const currentWeather = await this.fetchWeatherData(this.city);
+      const otherWeather = await this.fetchWeatherData(city);
+
+      if (currentWeather.main.temp > otherWeather.main.temp) {
+        console.log(`${this.city} is warmer than ${city}`);
+      } else if (currentWeather.main.temp < otherWeather.main.temp) {
+        console.log(`${city} is warmer than ${this.city}`);
+      } else {
+        console.log(`${this.city} and ${city} have the same temperature`);
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 }
-  // async refreshWeatherData() {
-  //   try {
-  //     const weatherData = await this.fetchWeatherData(this.city);
-  //     this.weatherUI.displayWeather(weatherData);
-  //   } catch (error) {
-  //     console.error(error.message);
-  //   }
-  // }
-
-  // async compareWith(city) {
-  //   try {
-  //     const currentWeather = await this.fetchWeatherData(this.city);
-  //     const otherWeather = await this.fetchWeatherData(city);
-
-  //     if (currentWeather.main.temp > otherWeather.main.temp) {
-  //       console.log(`${this.city} is warmer than ${city}`);
-  //     } else if (currentWeather.main.temp < otherWeather.main.temp) {
-  //       console.log(`${city} is warmer than ${this.city}`);
-  //     } else {
-  //       console.log(`${this.city} and ${city} have the same temperature`);
-  //     }
-  //   } catch (error) {
-  //     console.error(error.message);
-  //   }
-  // }
-// }
 
 module.exports = Weather;
 
-// async function main() {
-//   const client = new WeatherClient();
-//   const weather = new Weather(client);
-//   await weather.load('Bristol');
-//   const weatherData = weather.getWeatherData();
-//   console.log('weatherData:', weatherData);
-// }
-// main()
+async function main() {
+  const client = new WeatherClient();
+  const weather = new Weather(client);
+  await weather.load('Bristol');
+  const weatherData = weather.getWeatherData();
+  console.log('weatherData:', weatherData);
+}
+main()
